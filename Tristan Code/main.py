b1dd4533d4c59776b9fb6cd6cb2feec03352c0b5
Jabsalot <124105647+Jabsalot@users.noreply.github.com>
2023-11-21 14:50:03 -0700

    Update main.py

--- Tristan Code/main.py
+++ Tristan Code/main.py
@@ -6,13 +6,5 @@ from Project.classroomGUI import *
 #   - When GUI is fully functioning work on back end classLogic
 #############################################################################################
 
-#############################################################################################
-# TODO
-#   - Finish development of frames (removeStudent and TakeAttendance)
-#   - Finish development of HideFrame method (Add hiding for frames remove and TakeAttendance)
-#   - Start development on defaultFrame button commands (Should only be one method)
-#   - START ON BACKEND
-#############################################################################################
-
 # ---------- MAIN CODE RAN HERE ---------- #
 classGUI = ClassroomGUI()
